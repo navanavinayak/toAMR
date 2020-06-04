8b116f92819e8e13d30bc3c665e293706112ca22
Vinayak <vinayak@navanatech.in>
2020-06-04 15:18:57 +0530

    Fixed filename space issue

--- audioConvert.sh
+++ audioConvert.sh
@@ -11,16 +11,13 @@ ls $search_dir | while read entry; do
 	out_dir=`echo "$output_dir/$entry"`
 	`mkdir -p $out_dir`
 	subdir=`echo "$search_dir/$entry"`
-	ls $subdir | while read fileentry; do
+	ls "$subdir"/* | while read fileentry; do
 		echo '.'
-		echo $fileentry
 		outfile=`echo "$out_dir/$(basename "$fileentry" | sed 's/\(.*\)\..*/\1/').amr"`
-		echo $outfile
-		infile=`echo $subdir/"$fileentry"`
-		echo $infile
-		`bin/ffmpeg -y -i "$infile" -ar 16000 -ab 19.85k -ac 1  -loglevel quiet -stats -acodec libvo_amrwbenc "$outfile"`
-		outfile=""	
+		infile=`echo "$subdir/$(basename "$fileentry")"`
+		echo "Converting $infile"
+		`bin/ffmpeg -y -i "$infile" -ar 16000 -ab 19.85k -ac 1  -loglevel quiet -stats -nostdin -acodec libvo_amrwbenc "$outfile"`
 	done
 	echo '.'
-i	echo '.'
+	echo '.'
 done
